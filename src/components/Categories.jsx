7f8a0e49e81c495d6321bc997fd339e6ca4a7906
vikki-viks <vicka.kudryavtseva@yandex.ru>
2021-05-03 21:03:07 +0300

    added useState

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,31 @@
-function Categories({items, onClickItem}) {
+import React from 'react';
+
+function Categories({ items, onClickItem }) {
+  const [activeItem, setActiveItem] = React.useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  };
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        {items.map((name,index) => < li onClick={() => onClickItem(name)} key={`${name}_${index}`}>{name}</li>)}
+        <li
+          className={activeItem === null ? 'active' : ''}
+          onClick={() => onSelectItem(null)}
+        >
+          Все
+        </li>
+        {items &&
+          items.map((name, index) => (
+            <li
+              className={activeItem === index ? 'active' : ''}
+              onClick={() => onSelectItem(index)}
+              key={`${name}_${index}`}
+            >
+              {name}
+            </li>
+          ))}
       </ul>
     </div>
   );
