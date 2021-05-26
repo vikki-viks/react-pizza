c016a47c9d3ba243f5801a3fddc011d5e2e934da
vikki-viks <vicka.kudryavtseva@yandex.ru>
2021-05-26 16:50:39 +0300

    create Cart and Home, added route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./scss/app.scss";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './scss/app.scss';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App></App>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
