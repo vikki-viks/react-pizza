790a1598c05b0be122dc8967fa485489563987ba
vikki-viks <vicka.kudryavtseva@yandex.ru>
2021-05-27 21:34:00 +0300

    added json and new component PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React from 'react';
 import { Route } from 'react-router';
 import { Header } from './components';
 import { Home, Cart } from './pages';
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((resp) => resp.json())
+      .then((json) => {
+        setPizzas(json.pizzas);
+      });
+  }, []);
   return (
     <div className="wrapper">
       <Header></Header>
       <div className="content">
-        <Route path="/" component={Home} exact></Route>
+        <Route
+          path="/"
+          render={() => <Home items={pizzas}></Home>}
+          exact
+        ></Route>
         <Route path="/cart" component={Cart} exact></Route>
       </div>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from './Button';
 import Categories from './Categories';
 import Header from './Header';
 import SortPopup from './SortPopup';
+import PizzaBlock from './PizzaBlock';
 
-export { Button, Header, Categories, SortPopup };
+export { Button, Header, Categories, SortPopup, PizzaBlock };
